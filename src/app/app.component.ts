6001dbf0d8cd428726ce60713cd2239881d38b93
marrshaaa <marrshaaa@gmail.com>
2018-10-08 15:20:01 +0300

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ToDo-app';
+export class AppComponent implements OnInit{
+    todoForm: FormGroup;
+    length: string;
+
+    get TodoArray() {
+        return this.todoForm.get('todo') as FormArray;
+    }
+
+    ngOnInit() {
+        this.todoForm = new FormGroup({
+            addInput: new FormControl(''),
+            todo: new FormArray([]),
+        });
+
+    }
+
+    fillForm() {
+        return new FormControl(this.todoForm.value.addInput);
+    }
+
+    add() {
+        this.TodoArray.push(this.fillForm());
+        this.length = `${this.TodoArray.value.length} items length`;
+        console.log(this.TodoArray);
+    }
+
+    deleteTodo(i: number) {
+        this.TodoArray.removeAt(i);
+        this.length = `${this.TodoArray.value.length} items length`;
+        console.log(this.TodoArray.removeAt(i));
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AllModule} from './all/all.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AllModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
